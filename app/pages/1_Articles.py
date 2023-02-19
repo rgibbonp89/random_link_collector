4325e23828b9bae799466287852660e55645c0c3
Richard <rtprice1989@gmail.com>
2023-02-19 19:03:39 +0100

    added db connection

--- app/pages/1_Articles.py
+++ app/pages/1_Articles.py
@@ -1,36 +1,48 @@
 import streamlit as st
-
+from google.cloud import firestore
+import os
 from pages.pages_utils.search_bar import local_css, remote_css
 
 st.set_page_config(page_title="Articles")
 
+db = firestore.Client.from_service_account_json(os.environ.get("FS_KEY"))
+doc_ref = db.collection("articles")
+
+docs = doc_ref.stream()
+
 recent_tab, search_tab, all_tab = st.tabs(["Recent", "Search", "All"])
 
-list_in_first_tab = [
-    "Do riots affect voting habits?",
-    "Does GDP correlate with civil war?",
-    "FT editorial article",
-]
+list_in_first_tab = [doc for doc in docs]
+
+## make functions out of the below
+## create text entry
+## how will it update the firestore DB? post request?
+## in the initial chatgpt entry, async/await entry into the firestore DB?
+## what tech is needed for this?
+
+
+def example_callback(doc_ref, text):
+    return doc_ref.document("dUmVNAYWPEd9B8kpNllC").put
 
 
 with recent_tab:
     for l in list_in_first_tab:
-        with st.expander(l):
+        with st.expander(l.to_dict()["Name"]):
             with st.container():
-                edit = st.radio(f"""Edit: "{l}"?""", [True, False], index=1)
+                edit = st.radio(
+                    f"""Edit: "{l.to_dict()["Name"]}"?""", [True, False], index=1
+                )
                 if edit:
-                    with st.form("Edit article contents"):
-                        st.text_input("Article name: ", "Bla")
-                        st.text_input("Article url: ", "www.google.com")
-                        st.text_input("Auto-summary: ", "Text")
-                        st.text_input("My summary: ", "Text")
+                    with st.form(f"""Edit {l.to_dict()["Name"]}"""):
+                        st.text_input("Article name: ", value=l.to_dict()["Name"])
+                        st.text_input("Article url: ", value=l.to_dict()["URL"])
+                        st.text_area("Auto-summary: ", l.to_dict()["AutoSummary"])
                         submit_changes = st.form_submit_button("OK")
-                else:
-                    st.write("Article name: ", "Bla")
-                    st.write("Article url: ", "www.google.com")
-                    st.write("Auto-summary: ", "Text")
-                    st.write("My summary: ", "Text")
 
+                else:
+                    st.write("Article name: ", l.to_dict()["Name"])
+                    st.write("Article url: ", l.to_dict()["URL"])
+                    st.write("Auto-summary: ", l.to_dict()["AutoSummary"])
 
 with search_tab:
     local_css("./app/pages/style.css")
--- app/submission_form/submission_form.py
+++ app/submission_form/submission_form.py
@@ -1,5 +1,12 @@
 """Starting point for app submission form"""
 import streamlit as st
+from google.cloud import firestore
+import os
+import string
+import random
+
+
+db = firestore.Client.from_service_account_json(os.environ.get("FS_KEY"))
 
 
 def create_text_submission_form() -> None:
@@ -7,10 +14,14 @@ def create_text_submission_form() -> None:
         st.write("Save new article")
         url_input = st.text_input("Article URL")
         name_input = st.text_input("Article name")
+        my_notes = st.text_area("My notes")
         submitted = st.form_submit_button("Submit")
+
         if submitted:
+            # Once the user has submitted, upload it to the database
             st.write("URL: ", url_input)
             st.write("Name: ", name_input)
-            st.write("Chat-GPT Summary: ", "Some random text about the article for now")
-            st.text_input("My notes")
-            final_submission = st.form_submit_button("Final submission")
+            doc_ref = db.collection("articles").document(
+                "".join(random.choice(string.ascii_uppercase) for _ in range(15))
+            )
+            doc_ref.set({"Name": name_input, "AutoSummary": my_notes, "URL": url_input})
