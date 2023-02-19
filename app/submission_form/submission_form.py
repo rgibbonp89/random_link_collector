927c134dc63573e83d53e0d5113a64d82650a29b
Richard <rtprice1989@gmail.com>
2023-02-19 21:08:32 +0100

    not working with docker yet

--- app/submission_form/submission_form.py
+++ app/submission_form/submission_form.py
@@ -4,9 +4,17 @@ from google.cloud import firestore
 import os
 import string
 import random
+import openai
 
 
 db = firestore.Client.from_service_account_json(os.environ.get("FS_KEY"))
+openai.api_key = os.environ.get("OPENAI_KEY")
+
+# Set up the model and prompt
+MODEL_ENGINE = "text-davinci-003"
+MAX_TOKENS = 4000
+TEMPERATURE = 0.1
+ID_LENGTH = 15
 
 
 def create_text_submission_form() -> None:
@@ -14,14 +22,30 @@ def create_text_submission_form() -> None:
         st.write("Save new article")
         url_input = st.text_input("Article URL")
         name_input = st.text_input("Article name")
-        my_notes = st.text_area("My notes")
         submitted = st.form_submit_button("Submit")
 
         if submitted:
             # Once the user has submitted, upload it to the database
             st.write("URL: ", url_input)
             st.write("Name: ", name_input)
-            doc_ref = db.collection("articles").document(
-                "".join(random.choice(string.ascii_uppercase) for _ in range(15))
+            # This needs to be made async
+            completion = openai.Completion.create(
+                engine=MODEL_ENGINE,
+                prompt=f"Could you summarize this article {url_input}? You don't have to keep it short.",
+                n=1,
+                max_tokens=MAX_TOKENS,
+                temperature=TEMPERATURE,
             )
-            doc_ref.set({"Name": name_input, "AutoSummary": my_notes, "URL": url_input})
+            doc_ref = db.collection("articles").document(
+                "".join(
+                    random.choice(string.ascii_uppercase + string.ascii_lowercase)
+                    for _ in range(ID_LENGTH)
+                )
+            )
+            doc_ref.set(
+                {
+                    "Name": name_input,
+                    "AutoSummary": completion.choices[0].text,
+                    "URL": url_input,
+                }
+            )
