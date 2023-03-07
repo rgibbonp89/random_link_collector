b11a88148b40056fc6fe23c4549edd0e0e881d1e
Richard <rtprice1989@gmail.com>
2023-03-07 08:58:38 +0100

    RLC-37 serving static files in flask

--- backend/app.py
+++ backend/app.py
@@ -6,13 +6,13 @@ from backend.endpoints.existing_articles import articles_blue
 from backend.endpoints.submit import submit_blue
 
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_folder="build", static_url_path="/")
 app.register_blueprint(articles_blue)
 app.register_blueprint(submit_blue)
 
 CORS(app)
 
 
-@app.route("/main", endpoint="main")
+@app.route("/")
 def main():
-    pass
+    return app.send_static_file("index.html")
